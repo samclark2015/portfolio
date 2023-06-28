13ffd0310de44e6da9ab477efbdcdd71bdbe790d
Sam Clark <sclark@bnl.gov>
2023-06-28 12:19:53 -0500

    Attempt at custom theme

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "themes/**/*.html",
+    "content/**/*.md", 
+    "layouts/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
