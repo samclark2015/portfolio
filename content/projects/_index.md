89228a931c4e2c3a3ae22e2ecd7bbf779a146194
Sam Clark <sclark@bnl.gov>
2023-06-28 15:58:59 -0500

    Theme coming great

---
+++ content/projects/_index.md
@@ -0,0 +1,5 @@
+---
+title: Projects
+---
+
+This is a portfolio of my past and present projects.
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,15 @@
 module.exports = {
   content: [
     "themes/**/*.html",
-    "content/**/*.md", 
+    "content/**/*.md",
     "layouts/**/*.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ["-apple-system,BlinkMacSystemFont,segoe ui,Roboto,Oxygen-Sans,Ubuntu,Cantarell,helvetica neue,Helvetica,游ゴシック,pingfang sc,STXihei,华文细黑,microsoft yahei,微软雅黑,SimSun,宋体,Heiti,黑体,sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
